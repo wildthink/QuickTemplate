cfe45128bde819a4a3aa557ef256c024a72f11da
Jason Jobe <jason2010@jasonjobe.com>
2013-09-21 18:07:13 -0400

    Initial Commit

---
+++ QuickTemplate/QuickTemplate.h
@@ -0,0 +1,13 @@
+//
+//  QuickTemplate.h
+//  QuickTemplate
+//
+//  Created by Jason Jobe on 9/21/13.
+//  Copyright (c) 2013 Jason Jobe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface QuickTemplate : NSObject
+
+@end
---
+++ QuickTemplate/QuickTemplate.m
@@ -0,0 +1,13 @@
+//
+//  QuickTemplate.m
+//  QuickTemplate
+//
+//  Created by Jason Jobe on 9/21/13.
+//  Copyright (c) 2013 Jason Jobe. All rights reserved.
+//
+
+#import "QuickTemplate.h"
+
+@implementation QuickTemplate
+
+@end
